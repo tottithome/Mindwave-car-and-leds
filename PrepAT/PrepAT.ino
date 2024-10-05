3252d68d5fef2d73905b39f731bb579a59247ad7
Eduardo Totti Thomé <151791156+tottithome@users.noreply.github.com>
2024-10-05 13:39:44 -0300

    Add files via upload

---
+++ PrepAT/PrepAT.ino
@@ -0,0 +1,12 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(11, 10); // Rx/Tx
+
+void setup() {
+    BT.begin(38400);
+    Serial.begin(9600);
+}
+
+void loop() {
+    if (BT.available()) Serial.write(BT.read());
+    if (Serial.available()) BT.write(Serial.read());
+}
